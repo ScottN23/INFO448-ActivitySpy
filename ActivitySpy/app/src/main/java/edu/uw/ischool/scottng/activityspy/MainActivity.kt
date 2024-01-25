fe61e5f7b710ba00387cfbad101816772fb52c62
Scott Nguyen <scottn523@gmail.com>
2024-01-25 13:00:31 -0800

    finished w/o extra credit

---
+++ ActivitySpy/app/src/main/java/edu/uw/ischool/scottng/activityspy/MainActivity.kt
@@ -0,0 +1,39 @@
+package edu.uw.ischool.scottng.activityspy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("ActivitySpy", "onCreate event fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("ActivitySpy", "onDestroy event fired")
+        Log.i("ActivitySpy", "We're going down, Captain!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("ActivitySpy", "onStart event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("ActivitySpy", "onStop event fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("ActivitySpy", "onPause event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("ActivitySpy", "onResume event fired")
+    }
+}
